9f55b6887e8e663684092beaf6572b6f49e4a00f
andrei.nikulin <andrei.nikulin@omniva.ee>
2023-06-30 10:12:15 +0300

    SB2.7 - slightly fixed structure

--- src/main/java/com/example/demo/domain/AbstractTranslationEntity.java
+++ src/main/java/com/example/demo/domain/AbstractTranslationEntity.java
@@ -2,8 +2,6 @@ package com.example.demo.domain;
 
 import javax.persistence.Column;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
@@ -18,11 +16,7 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-public abstract class AbstractTranslationEntity<T extends AbstractEntity<Long> & WithTranslationKey> extends AbstractEntity {
-
-	@Id
-	@GeneratedValue
-	private Long id;
+public abstract class AbstractTranslationEntity<T extends AbstractEntity<Long> & WithTranslationKey> extends AbstractEntity<Long> {
 
 	@JoinColumn(name = "CLASSIFIER_ID", nullable = false)
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
@@ -31,8 +25,7 @@ public abstract class AbstractTranslationEntity<T extends AbstractEntity<Long> &
 	@Column(name = "LOCALE_CODE", length = 15, nullable = false)
 	private String localeCode;
 
-	AbstractTranslationEntity(Long id, T classifier, String localeCode) {
-		this.id = id;
+	AbstractTranslationEntity(T classifier, String localeCode) {
 		this.classifier = classifier;
 		this.localeCode = localeCode;
 	}
--- src/main/java/com/example/demo/domain/UserTranslation.java
+++ src/main/java/com/example/demo/domain/UserTranslation.java
@@ -1,6 +1,8 @@
 package com.example.demo.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Builder;
@@ -17,11 +19,15 @@ import lombok.ToString;
 @Table(name = "USER_TRANSLATION")
 public class UserTranslation extends AbstractTranslationEntity<User> {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+
 	private String translation;
 
 	@Builder
-	UserTranslation(Long id, User classifier, String localeCode, String translation) {
-		super(id, classifier, localeCode);
+	UserTranslation(User classifier, String localeCode, String translation) {
+		super(classifier, localeCode);
 		this.translation = translation;
 	}
 }
